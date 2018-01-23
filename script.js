f8af805b07a8f6e6a5fc0c0a8d95b6e4c286ee88
Lilian Pacaud <lilian.pacaud@supinternet.fr>
2018-01-23 23:38:33 +0100

    Show name and type of pokemons

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function(){
     document.querySelector('#form').onsubmit = function(){
         var search = document.querySelector('input[name="search"]').value;
         var imgPoke = document.querySelector('#imgPoke');
+        var stats = document.querySelector('#stats');
 
         $.ajax({
             url: 'list.json',
@@ -12,11 +13,12 @@ window.onload = function(){
                 for (var i in data) {
                     namePoke = data[i].name.toLowerCase();
                     if (search === namePoke || search === i){
-                        imgPoke.setAttribute('src', 'https://img.pokemondb.net/artwork/'+ namePoke +'.jpg');
+                        imgPoke.setAttribute('src', 'https://img.pokemondb.net/artwork/'+ namePoke +'.jpg')
+                        stats.innerHTML = data[i].name + '<br>' + data[i].type
                     }
                 }
             }
-    });
+        });
         return false
     };
 };
