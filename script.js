157951a70153a96dbcf8ac3b8731f3cd63d1ba27
Pakopac <lilian.pacaud@supinternet>
2018-01-25 13:49:28 +0100

    If search is empty, show nothing

--- script.js
+++ script.js
@@ -17,54 +17,55 @@ window.onload = function(){
             dataType: 'json',
             success: function (data) {
                 function isPokeExist() {
-                    for (var i in data){
+                    for (var i in data) {
                         var namePoke = data[i].name.toLowerCase();
-                        if (search === namePoke || search === i){
-                           return data[i]
+                        if (search === namePoke || search === i) {
+                            return data[i]
                         }
                     }
                 }
-
-                function linksImg(imgPoke, end){
-                    return imgPoke.setAttribute('src', 'https://img.pokemondb.net/artwork/' + isPokeExist().name.toLowerCase() + end);
-                }
-
-                function statsPoke(endName){
-                    return stats.innerHTML = isPokeExist().name + endName + '<br>' + isPokeExist().type;
-                }
-
-                if (typeof isPokeExist() !== 'undefined' && search != 29 && search != 32 && search !== 'nidoran') {
-                    linksImg(imgPoke, '.jpg');
-                    statsPoke('');
-                }
-
-                else if (typeof isPokeExist() !== 'undefined' && search === 'nidoran'){
-                    linksImg(imgPoke, '-m.jpg');
-                    linksImg(imgPoke2, '-f.jpg');
-                    statsPoke('');
-                }
-
-                else if (typeof isPokeExist() !== 'undefined' && search == 29){
-                    imgPoke2.setAttribute('src', 'https://img.pokemondb.net/artwork/' + isPokeExist().name.toLowerCase() + '-f.jpg');
-                    statsPoke('&#9792;');
-                }
-
-                else if (typeof isPokeExist() !== 'undefined' && search == 32){
-                    imgPoke.setAttribute('src', 'https://img.pokemondb.net/artwork/' + isPokeExist().name.toLowerCase() + '-m.jpg');
-                    statsPoke('&#9794;');
-                }
-
-                else {
-                    if (isNaN(search)) {
-                        blockError = search + ' not found'
+                if (search.length !== 0) {
+                    function linksImg(imgPoke, end) {
+                        return imgPoke.setAttribute('src', 'https://img.pokemondb.net/artwork/' + isPokeExist().name.toLowerCase() + end);
                     }
 
-                    if (!isNaN(search)) {
-                        blockError = 'Pokémon number ' + search + ' not found'
+                    function statsPoke(endName) {
+                        return stats.innerHTML = isPokeExist().name + endName + '<br>' + isPokeExist().type;
                     }
-                }
 
-                document.querySelector('#error').innerHTML = blockError;
+                    if (typeof isPokeExist() !== 'undefined' && search != 29 && search != 32 && search !== 'nidoran') {
+                        linksImg(imgPoke, '.jpg');
+                        statsPoke('');
+                    }
+
+                    else if (typeof isPokeExist() !== 'undefined' && search === 'nidoran') {
+                        linksImg(imgPoke, '-m.jpg');
+                        linksImg(imgPoke2, '-f.jpg');
+                        statsPoke('');
+                    }
+
+                    else if (typeof isPokeExist() !== 'undefined' && search == 29) {
+                        imgPoke2.setAttribute('src', 'https://img.pokemondb.net/artwork/' + isPokeExist().name.toLowerCase() + '-f.jpg');
+                        statsPoke('&#9792;');
+                    }
+
+                    else if (typeof isPokeExist() !== 'undefined' && search == 32) {
+                        imgPoke.setAttribute('src', 'https://img.pokemondb.net/artwork/' + isPokeExist().name.toLowerCase() + '-m.jpg');
+                        statsPoke('&#9794;');
+                    }
+
+                    else {
+                        if (isNaN(search)) {
+                            blockError = search + ' not found'
+                        }
+
+                        if (!isNaN(search)) {
+                            blockError = 'Pokémon number ' + search + ' not found'
+                        }
+                    }
+
+                    document.querySelector('#error').innerHTML = blockError;
+                }
             }
         });
         return false
